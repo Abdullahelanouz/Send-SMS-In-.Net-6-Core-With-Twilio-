90a1d6a824b52cd86629e97849740ab6432dc56a
AbdallaEbrahimElanoz <anaabdoa319@gmail.com>
2023-09-28 21:34:46 +0300

    Add Send SMS Endpoint

---
+++ Send SMS In .Net 6 (Core) With Twilio/Controllers/SMSController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Send_SMS_In_.Net_6__Core__With_Twilio.Dtos;
+using Send_SMS_In_.Net_6__Core__With_Twilio.Services;
+
+namespace Send_SMS_In_.Net_6__Core__With_Twilio.Controllers
+{
+   
+    
+        [Route("api/[controller]")]
+        [ApiController]
+        public class SMSController : ControllerBase
+        {
+            private readonly ISMSService _smsService;
+
+            public SMSController(ISMSService smsService)
+            {
+                _smsService = smsService;
+            }
+
+            [HttpPost("send")]
+            public IActionResult Send(SendSMSDto dto)
+            {
+                var result = _smsService.Send(dto.MobileNumber, dto.Body);
+
+                if (!string.IsNullOrEmpty(result.ErrorMessage))
+                    return BadRequest(result.ErrorMessage);
+
+                return Ok(result);
+            }
+        }
+    
+}
---
+++ Send SMS In .Net 6 (Core) With Twilio/Dtos/SendSMSDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Send_SMS_In_.Net_6__Core__With_Twilio.Dtos
+{
+    public class SendSMSDto
+    {
+        public string MobileNumber { get; set; }
+        public string Body { get; set; }
+    }
+}
