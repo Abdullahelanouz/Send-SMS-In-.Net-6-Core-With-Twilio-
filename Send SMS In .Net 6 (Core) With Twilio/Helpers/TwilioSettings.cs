16c1014652fed6b74623fb01bacc1fd5e44a133b
AbdallaEbrahimElanoz <anaabdoa319@gmail.com>
2023-09-28 20:27:44 +0300

    Add project files.

---
+++ Send SMS In .Net 6 (Core) With Twilio/Helpers/TwilioSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Send_SMS_In_.Net_6__Core__With_Twilio.Helpers
+{
+  
+        public class TwilioSettings
+        {
+            public string AccountSID { get; set; }
+            public string AuthToken { get; set; }
+            public string TwilioPhoneNumber { get; set; }
+        }
+}
+
---
+++ Send SMS In .Net 6 (Core) With Twilio/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.Extensions.DependencyInjection;
+using Send_SMS_In_.Net_6__Core__With_Twilio.Helpers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.Configure<TwilioSettings>(builder.Configuration.GetSection("Twilio"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
